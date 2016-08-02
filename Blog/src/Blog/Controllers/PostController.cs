aa6434f72f2364bbe3f2182e164506119e049706
Luan souza <lpaiva@avenuecode.com>
2016-08-02 16:19:14 -0300

    Blog part1

---
+++ Blog/src/Blog/Controllers/PostController.cs
@@ -0,0 +1,42 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class PostController: Controller
+    {
+
+        private static List<BlogPost> _posts = new List<BlogPost>
+        {
+            new BlogPost { Id = 1,Title = "Post1", CreatedAt = DateTime.Now,
+            Content = "Teste" },
+
+             new BlogPost { Id = 2,Title = "Post2", CreatedAt = DateTime.Now,
+            Content = "Teste2" }
+        };
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View(_posts);
+
+        }
+        public IActionResult Create(BlogPost post)
+        {
+            post.CreatedAt = DateTime.Now;
+            _posts.Add(post);
+
+            return RedirectToAction("Index"); 
+
+        }
+    }
+}
---
+++ Blog/src/Blog/Models/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Content { get; internal set; }
+    }   
+}
